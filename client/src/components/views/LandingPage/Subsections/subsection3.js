bcf5c7c8da4d7f45b527b0607c489208c4c19b40
Aman <amankh9079838225@gmail.com>
2021-05-29 21:45:48 +0530

    commit

--- client/src/components/views/LandingPage/Subsections/subsection3.js
+++ client/src/components/views/LandingPage/Subsections/subsection3.js
@@ -6,32 +6,34 @@ function SubSec3(){
     return <div class="suba">
 
 <p class="explore">Our catch phrase and moto here probably of two lines.....</p>
+<div class="m-5">
 <Row className="rowa">
   <Col>
-      <p>hell 1</p>
+      
   </Col>
   <Col className="highcola">
-    hell 2
+    
   </Col>
 </Row>
 
 <Row className="rowa">
   <Col className="highcola">
-    eighp3
+    
   </Col>
   <Col >
-      efofpho;of4
+    
   </Col>
 </Row>
 
 <Row className="rowa">
   <Col>
-    ewf;ho 5
+    
   </Col>
   <Col className="highcola">
-      efifh 6
+    
   </Col>
 </Row>
+</div>
 
     </div>
 }
