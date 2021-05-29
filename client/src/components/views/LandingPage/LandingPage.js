4cf1a31c77f99484ea36200e20eace9e130cabb2
Utkarsh Dyundi <meetmeutkarsh@gmail.com>
2021-05-29 21:33:59 +0530

    top2

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { FaCode } from "react-icons/fa";
 import Top from "./Subsections/top.js"
+import Top2 from "./Subsections/top2.js"
 function LandingPage() {
     return (
         <>
            <Top></Top>
+           <Top2 />
         </>
     )
 }
