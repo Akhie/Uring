a83aa83ff35a564cef54e347cd4ace8305139a78
Akhie <Akhilesh.yadav200093@gmail.com>
2021-05-29 19:53:12 +0530

    Bottom section

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { FaCode } from "react-icons/fa";
-import Top from "./Subsections/top.js"
+import Top from "./Subsections/top.js";
+import Bottom from "./Subsections/Bottom.js";
+
 function LandingPage() {
     return (
         <>
            <Top></Top>
+           <Bottom></Bottom>
         </>
     )
 }
