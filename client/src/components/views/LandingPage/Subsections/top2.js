3590ceeb6c5da0af225b05c82be090ac874141c7
Utkarsh Dyundi <meetmeutkarsh@gmail.com>
2021-06-02 17:54:26 +0530

    added animation

--- client/src/components/views/LandingPage/Subsections/top2.js
+++ client/src/components/views/LandingPage/Subsections/top2.js
@@ -7,24 +7,23 @@ function Top2(){
 <br /><br />
 <Row>
 <Col lg="6" sm="12">
-  <p class="explore2">Our catch phrase and moto here probably of two lines.....</p>
+  <p class="explore2">We help you find details of all the services available near you hastlefree. </p>
   <div class="explore3">
   <ul>
-  <li>adgfkadsgfk gaksdfgk dsag ak gfasd fkasd gfkas </li>
-  <li>adgfkadsgfk gaksdfgk dsag ak gfasd fkasd gfkas</li>
-  <li>adgfkadsgfk gaksdfgk dsag ak gfasd fkasd gfkas  </li>
-  <li>adgfkadsgfk gaksdfgk dsag ak gfasd fkasd gfkas</li>
-  <li>adgfkadsgfk gaksdfgk dsag ak gfasd fkasd gfkas  </li>
+  <li>Find service providers active near you.</li>
+  <li>Read reviews of them and choose one which suits you.</li>
+  <li>No extra fees.</li>
+  <li>No tension of searching different service at differnt place.</li>
 </ul>
 </div>
 </Col>
 
-<Col className="phone1" >
+<Col className="phone1" data-aos="flip-left" >
 
   <img class="pisa2" src="https://cdn.pixabay.com/photo/2017/10/07/21/37/iphone-2828016_960_720.png" alt=""></img>
 
 </Col>
-<Col  >
+<Col  data-aos="flip-right">
   <img class="pisa2" src="https://cdn.pixabay.com/photo/2017/10/07/21/37/iphone-2828016_960_720.png" alt=""></img>
 </Col>
 
